f15358fcc63e818d165d2476a06229845e784725
Manuel <manuelheller@outlook.com>
2021-01-24 19:45:56 +0100

    work on model evaluation

--- train_model.py
+++ train_model.py
@@ -9,6 +9,7 @@ import numpy as np
 import os
 import cv2
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
 
 ######################################################################
 # Set parameters
@@ -68,7 +69,8 @@ def prepare_data():
 
     ids = np.array([f'image_{i}' for i in range(1,31)])
     random.shuffle(ids)
-    cut_off = int(len(ids)*0.8)
+    # //MH update train/val split
+    cut_off = int(len(ids)*0.5)
     train = ids[:cut_off]
     val = ids[cut_off:]
 
@@ -243,14 +245,18 @@ def evaluate_model(eval_dl, model):
         yhat = model(inputs)
         # retrieve numpy array
         yhat = yhat.detach().numpy()
+        print(yhat.shape)
+        print(yhat)
         actual = targets.numpy()
+        print(actual.shape)
+        print(actual)
         actual = actual.reshape((len(actual), 1))
         # round to class values
         yhat = yhat.round()
         # store
         predictions.append(yhat)
         actuals.append(actual)
-    predictions, actuals = vstack(predictions), vstack(actuals)
+    predictions, actuals = np.vstack(predictions), np.vstack(actuals)
     # calculate accuracy
     acc = accuracy_score(actuals, predictions)
     return acc
@@ -266,8 +272,8 @@ model = UNet()
 # train the model
 train_model(train_dl, model)
 # evaluate the model
-#acc = evaluate_model(test_dl, model)
-#print('Accuracy: %.3f' % acc)
+acc = evaluate_model(eval_dl, model)
+print('Accuracy: %.3f' % acc)
 # make a single prediction (expect class=1)
 #row = [1,0,0.99539,-0.05889,0.85243,0.02306,0.83398,-0.37708,1,0.03760,0.85243,-0.17755,0.59755,-0.44945,0.60536,-0.38223,0.84356,-0.38542,0.58212,-0.32192,0.56971,-0.29674,0.36946,-0.47357,0.56811,-0.51171,0.41078,-0.46168,0.21266,-0.34090,0.42267,-0.54487,0.18641,-0.45300]
 #yhat = predict(row, model)
