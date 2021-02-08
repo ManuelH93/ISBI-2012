c0627bcda6ae1f97e53960e9724e29e94b28803c
Manuel <manuelheller@outlook.com>
2021-02-08 11:16:55 +0100

    functioning image transformaion

--- test.py
+++ test.py
@@ -1,67 +1,78 @@
-import os,sys
-import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-import helper
-import simulation
+import matplotlib.pyplot as plt
+import tifffile as tiff
+import os
+import albumentations as A
+import cv2
 
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, datasets, models
+# Define parameters
 
-import torchvision.utils
+DATA = 'raw_data'
+OUTPUT = 'output'
 
-from torchsummary import summary
-import torch
-import torch.nn as nn
-import pytorch_unet
+# Read in images and masks
 
-from collections import defaultdict
-import torch.nn.functional as F
-from loss import dice_loss
+imgs = tiff.imread(os.path.join(DATA,'train-volume.tif'))
+imgs = imgs.transpose(1,2,0)
+imgs = np.squeeze(np.dsplit(imgs, 30))
 
-import torch.optim as optim
-from torch.optim import lr_scheduler
-import time
-import copy
+masks = tiff.imread(os.path.join(DATA,'train-labels.tif'))
+masks = masks.transpose(1,2,0)
+masks = np.squeeze(np.dsplit(masks, 30))
+masks = masks / 255
 
-import math
+print(imgs.shape)
+print(masks.shape)
+
+plt.imshow(imgs[0])
+plt.show()
+plt.clf()
 
 
-# Generate some random images
-input_images, target_masks = simulation.generate_random_data(192, 192, count=4)
+plt.imshow(masks[0])
+plt.show()
+plt.clf()
 
-input_images = np.squeeze(input_images)
-print(input_images.shape)
+image = imgs[0]
+mask = masks[0]
 
-target_masks = np.squeeze(target_masks)
-print(target_masks.shape)
+def get_aug(p=1.0):
+    return A.Compose([
+        A.HorizontalFlip(),
+        A.VerticalFlip(),
+        A.RandomRotate90(),
+        A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=15, p=0.9, 
+                         border_mode=cv2.BORDER_REFLECT),
+        A.OneOf([
+            A.OpticalDistortion(p=0.3),
+            A.GridDistortion(p=.1),
+            A.IAAPiecewiseAffine(p=0.3),
+        ], p=0.3),
+        A.OneOf([
+            A.HueSaturationValue(10,15,10),
+            A.CLAHE(clip_limit=2),
+            A.RandomBrightnessContrast(),            
+        ], p=0.3),
+    ], p=p)
 
-#layer1 = target_masks[0]
-#layer2 = target_masks[1]
-#layer3 = target_masks[2]
-#layer4 = target_masks[0]
-#layer5 = target_masks[0]
-#layer6 = target_masks[0]
+tfms = get_aug()
+augmented = tfms(image=image, mask=mask)
+image, mask = augmented['image'],augmented['mask']
 
-#print('layer1')
-#print(layer1.min(), layer1.max(), layer1.mean(), layer1.std())
-#print(layer1[100:115,70:85])
-#print('layer2')
-#print(layer2.min(), layer2.max(), layer2.mean(), layer2.std())
-#print(layer2[100:115,70:85])
-#print('layer3')
-#print(layer3.min(), layer3.max(), layer3.mean(), layer3.std())
-#print(layer3[100:115,70:85])
-#print('layer4')
-#print(layer4.min(), layer4.max(), layer4.mean(), layer4.std())
-#print(layer4[100:115,70:85])
-#print('layer5')
-#print(layer5.min(), layer5.max(), layer5.mean(), layer5.std())
-#print(layer5[100:115,70:85])
-#print('layer6')
-#print(layer6.min(), layer6.max(), layer6.mean(), layer6.std())
-#print(layer6[100:115,70:85])
+plt.imshow(image)
+plt.show()
+plt.clf()
 
-#plt.imshow(target_masks)
-#plt.show()
-#plt.clf()
+plt.imshow(mask)
+plt.show()
+plt.clf()
+
+print(image.shape)
+print(mask.shape)
+
+
+
+
+
+
+x, y = zip(*[generate_img_and_mask(height, width) for i in range(0, count)])
