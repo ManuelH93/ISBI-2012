f69d22ab62ce91f90287b752cc4d480be652a10f
Manuel <manuelheller@outlook.com>
2021-02-25 13:22:44 +0100

    Add early stopping condition and figure for losses

--- temp/basic_unet.py
+++ temp/basic_unet.py
@@ -159,17 +159,23 @@ def print_metrics(metrics, epoch_samples, phase):
 def train_model(model, optimizer, scheduler, num_epochs=25):
     best_model_wts = copy.deepcopy(model.state_dict())
     best_loss = 1e10
+    early_stopping = False
+
+    # for figure
+    epochs = []
+    train_loss = []
+    val_loss = []
 
     for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
+        print('Epoch {}/{}'.format(epoch + 1, num_epochs))
+        epochs.append(epoch+1)
         
         since = time.time()
 
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
             if phase == 'train':
-                scheduler.step()
                 for param_group in optimizer.param_groups:
                     print("LR", param_group['lr'])
                     
@@ -197,22 +203,50 @@ def train_model(model, optimizer, scheduler, num_epochs=25):
                     if phase == 'train':
                         loss.backward()
                         optimizer.step()
+                        scheduler.step()
 
                 # statistics
                 epoch_samples += inputs.size(0)
 
             print_metrics(metrics, epoch_samples, phase)
             epoch_loss = metrics['loss'] / epoch_samples
+           
+            # collect statistics for figure
+            if phase == 'train':
+                train_loss.append(metrics['loss']/epoch_samples)
+            else:
+                val_loss.append(metrics['loss']/epoch_samples)
 
             # deep copy the model
             if phase == 'val' and epoch_loss < best_loss:
                 print("saving best model")
                 best_loss = epoch_loss
                 best_model_wts = copy.deepcopy(model.state_dict())
+                epochs_no_improve = 0
+            elif phase == 'val' and epoch_loss >= best_loss:
+                epochs_no_improve += 1
+                if epochs_no_improve == 100:
+                    print('Early stopping!')
+                    early_stopping = True
 
         time_elapsed = time.time() - since
         print('{:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+        if early_stopping == True:
+            break
+        else:
+            continue
     print('Best val loss: {:4f}'.format(best_loss))
+
+    # Save loss figure
+    plt.plot(epochs, train_loss, color='g', label = 'train')
+    plt.plot(epochs, val_loss, color='orange', label = 'test')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Losses')
+    plt.legend(loc="upper left")
+    #plt.show()
+    plt.savefig(os.path.join(OUTPUT, 'losses.png'))
+    plt.clf()
 
     # load best model weights
     model.load_state_dict(best_model_wts)
