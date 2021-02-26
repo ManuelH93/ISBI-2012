cff6791b347acb2079b2e9147d361800be90be0e
Manuel <manuelheller@outlook.com>
2021-02-26 21:05:49 +0100

    Update simulation script to work on submission

--- pred.py
+++ pred.py
@@ -9,7 +9,7 @@ import cv2
 import copy
 
 MODEL = 'trained_model'
-MODEL_VERSION = '2021.02.25_v2_and_graph_stop'
+MODEL_VERSION = '2021.02.24_and_model'
 
 DATA = 'processed_data'
 TEST = 'test'
--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ import numpy as np
 import albumentations as A
 import cv2
 import copy
+import torch
 
 def oned_to_twod(image):
     image = np.expand_dims(image, 0)
@@ -34,7 +35,7 @@ def get_aug_train(p=1.0):
 
 def get_aug_test(p=1.0):
     return A.Compose([
-        A.PadIfNeeded(min_height=572, min_width=572, p=1),
+        A.PadIfNeeded(min_height=696, min_width=696, p=1),
     ], p=p)
 
 def center_crop(image):
@@ -43,3 +44,22 @@ def center_crop(image):
     delta = array_size - target_size
     delta = delta // 2
     return image[delta:array_size-delta, delta:array_size-delta]
+
+def dim_plus_two(image):
+    image = np.expand_dims(image, 0)
+    image = np.expand_dims(image, 0)
+    return image
+
+def crop(images):
+    all_crops = []
+    for image in images:
+        image = np.squeeze(image)
+        crop_1 = dim_plus_two(image[0:572, 0:572])
+        crop_2 = dim_plus_two(image[0:572, 124:696])
+        crop_3 = dim_plus_two(image[124:696, 0:572])
+        crop_4 = dim_plus_two(image[124:696, 124:696])
+        image_crops = np.concatenate((crop_1, crop_2, crop_3, crop_4))
+        all_crops.append(image_crops)
+    all_crops = np.concatenate(all_crops)  
+    all_crops = torch.from_numpy(all_crops.astype(np.float32, copy=False))  
+    return all_crops
